d799f4aa5fff513d8bc7ddd03adc85c959a559a5
Simon Bull <simon.bull.84@googlemail.com>
2017-10-16 08:57:56 +0100

    Use different DB by default

--- tools/createPasswordResetV1.js
+++ tools/createPasswordResetV1.js
@@ -1,6 +1,8 @@
 const kue = require('kue');
 
-const queue = kue.createQueue();
+const queue = kue.createQueue({
+  redis: 'redis://127.0.0.1:6379?db=1'
+});
 
 const job = queue.create('passwordreset_v1', {
   email: 'user.one@dev.test',
