24e1e0e2c00e801d4fa56537373a38e9b75bcc24
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-10-19 16:12:51 +0100

    Update tools script

--- tools/createPasswordResetV1.js
+++ tools/createPasswordResetV1.js
@@ -6,7 +6,8 @@ const queue = kue.createQueue({
 
 const job = queue.create('passwordreset_v1', {
   email: 'user.one@dev.test',
-  code: 'A1B2C3D4'
+  code: 'A1B2C3D4',
+  clientId: 'foo1'
 }).save((err) => {
   if (err) {
     console.error(err);
