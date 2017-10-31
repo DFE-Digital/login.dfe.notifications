fe56bcf272fbe9cf4e8f8d0b6f0707865d37caa1
Simon Bull <simon.bull.84@googlemail.com>
2017-10-31 10:26:55 +0000

    Return url for reset password

--- src/app/passwordReset/passwordResetV1.js
+++ src/app/passwordReset/passwordResetV1.js
@@ -1,10 +1,13 @@
 const Email = require('./../../infrastructure/email');
+const config = require('./../../infrastructure/config')();
+const uuid = require('uuid/v4');
 
 const processor = async (job) => {
   const email = new Email();
   await email.send(job.email, 'password-reset', {
     code: job.code,
     clientId: job.clientId,
+    returnUrl: `${config.hostingEnvironment.interactionsUrl}/${uuid()}/resetpassword/confirm`,
   }, 'Password reset');
 };
 
