a07d689d8bdbf7b54b53101286ad6e16a7b51ec8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-11-10 14:09:34 +0000

    Register processor for the invitation message

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const logger = require('./infrastructure/logger');
 const Monitor = require('./app/monitor');
 const passwordReset = require('./app/passwordReset');
+const invitation = require('./app/invitation');
 
 const processorMappings = [];
 passwordReset.registerProcessors(processorMappings);
+invitation.registerProcessors(processorMappings);
 
 const monitor = new Monitor(processorMappings);
 monitor.start();
